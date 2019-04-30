dd8b974b2ed51c5c2ec39587d989874a52907ff7
Taylor Tyson1 <ttay921@live.unc.edu>
2019-04-29 20:53:47 -0400

    Initializing Site

---
+++ precache-manifest.43144ddaeedf9ea45fbe0e1f2ded0300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd42c7c957c4ae25b640f55aa402eeb3",
+    "url": "/rollingstones/index.html"
+  },
+  {
+    "revision": "560e888e5e051ca0654d",
+    "url": "/rollingstones/static/css/2.f283dd5a.chunk.css"
+  },
+  {
+    "revision": "11c98c460b337f73a815",
+    "url": "/rollingstones/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "560e888e5e051ca0654d",
+    "url": "/rollingstones/static/js/2.ef124c16.chunk.js"
+  },
+  {
+    "revision": "11c98c460b337f73a815",
+    "url": "/rollingstones/static/js/main.999161c2.chunk.js"
+  },
+  {
+    "revision": "78342d32e0257f0385c4",
+    "url": "/rollingstones/static/js/runtime~main.8a9dda7f.js"
+  }
+]);
